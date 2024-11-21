91ccfe471c1aae823c58451764a9be3bb27f8127
Felix Idén <ouc98oec@students.codeinstitute.net>
2024-11-21 13:53:42 +0000

    Add summernote

--- bikeblog/models.py
+++ bikeblog/models.py
@@ -16,7 +16,7 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
-    
+
     class Meta:
         ordering = ["-created_on"]
 
@@ -27,8 +27,8 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE,
                              related_name="comments")
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
@@ -37,4 +37,4 @@ class Comment(models.Model):
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.body} by {self.author}"
+        return f"Comment {self.body} by {self.name}"
--- codestar/urls.py
+++ codestar/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
-    path("", include("bikeblog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
+    path("", include("bikeblog.urls"), name="blog-urls"),
+    path('summernote/', include('django_summernote.urls')),
 ]
