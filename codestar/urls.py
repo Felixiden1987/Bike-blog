48ecb9cd52080b9b61b7c6e80c589a78646638e3
Felix Idén <ouc98oec@students.codeinstitute.net>
2024-11-23 17:35:54 +0000

    added authentication

--- codestar/urls.py
+++ codestar/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("about/", include("about.urls"), name="about-urls"),
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path("", include("bikeblog.urls"), name="blog-urls"),
     path('summernote/', include('django_summernote.urls')),
