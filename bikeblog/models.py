786ae965e28b9a29887f100b5add678ec88a0000
Felix Idén <ouc98oec@students.codeinstitute.net>
2024-11-24 13:29:33 +0000

    Add edit comments to users

--- bikeblog/models.py
+++ bikeblog/models.py
@@ -25,8 +25,8 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE,
                              related_name="comments")
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
@@ -35,4 +35,4 @@ class Comment(models.Model):
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.body} by {self.name}"
+        return f"Comment {self.body} by {self.author}"
